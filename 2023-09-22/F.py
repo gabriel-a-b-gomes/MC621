5809368972e0d6211f42d2b958b6ae2e20de991f
gabriel-a-b-gomes <gabriel.bertolino214@gmail.com>
2023-09-22 17:36:40 -0300

    fix

--- 2023-09-22/F.py
+++ 2023-09-22/F.py
@@ -1,8 +1,15 @@
+import sys
+
+if sys.version_info[0] < 3:
+    input_func = raw_input
+else:
+    input_func = input
+
 def main():
-  t = int(input())
+  t = int(input_func())
 
   for _ in range(t):
-    a, b, n, M = map(int, input().split())
+    a, b, n, M = map(int, input_func().split())
 
     i = 0
     F = 1
